454865b18e108607a0addbaf760a9506b9a1e4d5
Konrad5 <konradkc@op.pl>
2016-06-12 13:40:15 +0200

    DODANIE KOMENTARZY

--- frontend/controller.js
+++ frontend/controller.js
@@ -1,17 +1,24 @@
 var projektApp = angular.module('projektApp', []);
 
 
+// STWORZENIE DOMYŚLNEJ LISTY ZADAŃ
+
 var tasks = [
     {name: "zrobic zakupy", done: false},
     {name: "posprzatac dom", done: true},
     {name: "wyprowadzic psa", done: false}
 ];
 
+// STWORZENIE KONTROLERA
+
 projektApp.controller("projektCtrl", function ($scope) {
 
+// UDOSTĘPNIENIE ELEMENTÓW Z LISTY tasks DO WIDOKU
 
     $scope.zadania = tasks;
     //$scope.added = "";
+
+//FUNKCJA UMOŻLIWIAJĄCA DODANIE ZADANIA DO LISTY
 
     $scope.dodajZadanie = function () {
         if ($scope.added.length !== 0) {
@@ -19,6 +26,7 @@ projektApp.controller("projektCtrl", function ($scope) {
         }
     };
 
+//FUNKCJA UMOŻLIWIAJĄCA USUNIĘCIE ZADANIA Z LISTY
 
     $scope.usunZadanie = function (zadanie) {
         var index = $scope.zadania.indexOf(zadanie);
